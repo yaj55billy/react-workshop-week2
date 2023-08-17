90660ede757c08687f19f981652e57f28c4730bb
yaj55billy <yaj55ushydna@gmail.com>
2023-08-17 14:06:30 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+const App = () => {
+  return (
+    <>
+      <div className="container mx-auto">
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+      </div>
+      
+    </>
+  )
+}
+
+export default App;
