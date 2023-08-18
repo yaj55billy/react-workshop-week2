dbf3166c90121e853885ec284ed9009279bbfa6d
yaj55billy <yaj55ushydna@gmail.com>
2023-08-18 21:40:38 +0800

    程式(70%)

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 const data = [
 	{
 		id: 1,
@@ -50,18 +51,91 @@ const data = [
 ];
 
 const App = () => {
+	const [menu] = useState(data); // 原始 menu
+	const [cart, setCart] = useState([]); // 購物車資料
+	const [total, setTotal] = useState(0); // 總計
+	const [note, setNote] = useState(""); // 備註
+	const [order, setOrder] = useState([]); // 訂單
+
+	const addCart = (item) => {
+		const conformIndex = cart.findIndex((cartItem) => item.id === cartItem.id);
+
+		if (conformIndex === -1) {
+			// -1 表示購物車還沒有這個品項
+			const tempCart = [
+				...cart, // [] 淺拷貝
+				{
+					...item,
+					qty: 1, // 數量預設為 1
+				},
+			];
+			setCart(tempCart);
+		} else {
+			const tempCart = cart.map((cartItem) => {
+				return item.id === cartItem.id
+					? {
+							...cartItem,
+							qty: cartItem.qty < 10 ? cartItem.qty + 1 : cartItem.qty,
+					  }
+					: { ...cartItem };
+			});
+			setCart(tempCart);
+		}
+	};
+
+	const updateCart = (item, value) => {
+		const tempCart = cart.map((cartItem) => {
+			return item.id === cartItem.id
+				? {
+						...cartItem,
+						qty: value,
+				  }
+				: { ...cartItem };
+		});
+		setCart(tempCart);
+	};
+
+	const deleteCartItem = (item) => {
+		const tempCart = cart.filter((cartItem) => {
+			return item.id !== cartItem.id;
+		});
+		setCart(tempCart);
+	};
+
+	const createOder = () => {
+		// const tempOrder = [
+		// 	...order, // [] 淺拷貝
+		// 	{
+		// 		...item,
+		// 		qty: 1, // 數量預設為 1
+		// 	},
+		// ];
+		// setOrder(tempOrder);
+	};
+
+	useEffect(() => {
+		const totalPrice = cart.reduce((prev, curr) => {
+			return prev + curr.price * curr.qty;
+		}, 0);
+		setTotal(totalPrice);
+	}, [cart]);
+
 	return (
 		<div className="bg-[#F8F6F2] min-h-screen">
-			<div className="container mx-auto py-8">
+			<div className="container mx-auto px-3 py-8">
 				<div className="grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-8">
 					<div className="">
 						{/* 菜單 */}
 						<h2 className="text-2xl font-bold text-primary">選擇飲品</h2>
 						<hr className="mt-2 mb-4" />
 						<ul className="flex flex-wrap -mx-3">
-							{data.map((item) => {
+							{menu.map((item) => {
 								return (
-									<li key={item.id} className="md:w-1/2 w-full px-3 pb-6">
+									<li
+										key={item.id}
+										className="md:w-1/2 w-full px-3 pb-6"
+										onClick={() => addCart(item)}
+									>
 										<div className="rounded-lg border-2 p-3 cursor-pointer border-brown-light hover:bg-brown-100">
 											<h3 className="text-xl text-brown font-medium">
 												{item.name} <span>${item.price}</span>
@@ -79,96 +153,104 @@ const App = () => {
 						{/* 購物車 */}
 						<h2 className="text-2xl font-bold text-primary">購物列表</h2>
 						<hr className="mt-2 mb-4" />
-						<div className="rounded-lg border-2 border-brown-light mt-4 p-4 text-center text-brown font-bold">
-							尚未選擇飲品～
-						</div>
-						<div className="rounded-lg border-2 border-brown-light mt-4 p-4">
-							<table className="min-w-full leading-normal">
-								<thead>
-									<tr>
-										<th className="p-3 text-left text-xs font-semibold text-brown-light">
-											飲品資訊
-										</th>
-										<th className="p-3 text-center text-xs font-semibold text-brown-light">
-											數量
-										</th>
-										<th className="p-3 text-center text-xs font-semibold text-brown-light">
-											單價
-										</th>
-										<th className="p-3 text-center text-xs font-semibold text-brown-light">
-											小計
-										</th>
-									</tr>
-								</thead>
-								<tbody>
-									{[1, 2, 3].map((item) => {
-										return (
-											<tr key={item}>
-												<td className="p-3 border-b border-dashed border-brown-light">
-													<h3 className="text-xl text-brown font-medium">
-														四季春茶
-													</h3>
-													<p className="mt-1 text-md text-brown">
-														香醇四季春茶，回甘無比
-													</p>
-													<button
-														type="button"
-														className="mt-2 text-red-400 text-sm"
-													>
-														刪除品項
-													</button>
-												</td>
-												<td className="p-3 border-b border-dashed border-brown-light text-center text-md text-brown w-32">
-													<select
-														name="HeadlineAct"
-														id="HeadlineAct"
-														className="w-1/2 h-[32px] rounded-md text-center bg-brown-100 focus:outline-none focus:border-brown-dark"
-													>
-														<option value="1">1</option>
-														<option value="2">2</option>
-														<option value="3">3</option>
-														<option value="4">4</option>
-														<option value="5">5</option>
-														<option value="6">6</option>
-														<option value="7">7</option>
-														<option value="8">8</option>
-														<option value="9">9</option>
-														<option value="10">10</option>
-													</select>
-												</td>
-												<td className="p-3 border-b border-dashed border-brown-light text-center text-md text-brown">
-													50
-												</td>
-												<td className="p-3 border-b border-dashed border-brown-light text-center text-md text-brown">
-													100
-												</td>
-											</tr>
-										);
-									})}
-								</tbody>
-								<tfoot>
-									<tr>
-										<td colSpan="3"></td>
-										<td colSpan="1" className="p-3">
-											<h3 className="text-center text-xl text-brown font-medium">
-												總計: ${500}
-											</h3>
-										</td>
-									</tr>
-								</tfoot>
-							</table>
-							<textarea
-								id="OrderNotes"
-								className="mt-1 w-full rounded-lg border border-brown-light p-3 focus:outline-none focus:border-brown-dark"
-								rows="2"
-								placeholder="備註"
-							></textarea>
-							<div className="text-right">
-								<button className="inline-block mt-1 px-4 py-2 w-[150px] text-md font-medium text-white bg-brown-light hover:bg-brown">
-									送出訂單
-								</button>
+						{cart.length === 0 ? (
+							<div className="rounded-lg border-2 border-brown-light mt-4 p-4 text-center text-brown font-bold">
+								尚未選擇飲品～
 							</div>
-						</div>
+						) : (
+							<div className="rounded-lg border-2 border-brown-light mt-4 p-4">
+								<table className="min-w-full leading-normal">
+									<thead>
+										<tr>
+											<th className="p-3 text-left text-xs font-semibold text-brown-light">
+												飲品資訊
+											</th>
+											<th className="p-3 text-center text-xs font-semibold text-brown-light">
+												數量
+											</th>
+											<th className="p-3 text-center text-xs font-semibold text-brown-light">
+												單價
+											</th>
+											<th className="p-3 text-center text-xs font-semibold text-brown-light">
+												小計
+											</th>
+										</tr>
+									</thead>
+									<tbody>
+										{cart.map((item) => {
+											return (
+												<tr key={item.id}>
+													<td className="p-3 border-b border-dashed border-brown-light">
+														<h3 className="text-xl text-brown font-medium">
+															{item.name}
+														</h3>
+														<p className="mt-1 text-md text-brown">
+															{item.description}
+														</p>
+														<button
+															type="button"
+															className="mt-2 text-red-400 text-sm"
+															onClick={() => deleteCartItem(item)}
+														>
+															刪除品項
+														</button>
+													</td>
+													<td className="p-3 border-b border-dashed border-brown-light text-center text-md text-brown w-32">
+														<select
+															name=""
+															id=""
+															className="w-1/2 h-[32px] rounded-md text-center bg-brown-100 focus:outline-none focus:border-brown-dark"
+															value={item.qty}
+															onChange={(e) => updateCart(item, e.target.value)}
+														>
+															{[...Array(10).keys()].map((item) => {
+																return (
+																	<option value={item + 1} key={item}>
+																		{item + 1}
+																	</option>
+																);
+															})}
+														</select>
+													</td>
+													<td className="p-3 border-b border-dashed border-brown-light text-center text-md text-brown">
+														{item.price}
+													</td>
+													<td className="p-3 border-b border-dashed border-brown-light text-center text-md text-brown">
+														{item.price * item.qty}
+													</td>
+												</tr>
+											);
+										})}
+									</tbody>
+									<tfoot>
+										<tr>
+											<td colSpan="3"></td>
+											<td colSpan="1" className="p-3">
+												<h3 className="text-center text-xl text-brown font-medium">
+													總計: ${total}
+												</h3>
+											</td>
+										</tr>
+									</tfoot>
+								</table>
+								<textarea
+									id="OrderNotes"
+									className="mt-1 w-full rounded-lg border border-brown-light p-3 focus:outline-none focus:border-brown-dark"
+									rows="2"
+									placeholder="備註"
+									value={note}
+									onChange={(e) => setNote(e.target.value)}
+								></textarea>
+								<div className="text-right">
+									<button
+										className="inline-block mt-1 px-4 py-2 w-[150px] text-md font-medium text-white bg-brown-light hover:bg-brown"
+										onClick={() => createOder()}
+									>
+										送出訂單
+									</button>
+								</div>
+							</div>
+						)}
 					</div>
 				</div>
 
